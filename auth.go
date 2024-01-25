801a952d286a5674821791bf282c50b1f75c88ea
krechmer <krech52@yandex.ru>
2024-01-25 16:43:02 +0000

    Skip password

--- auth.go
+++ auth.go
@@ -18,9 +18,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type Body struct {
+type BodyWithPass struct {
 	Username	string		`json:"username"`
 	Password	string		`json:"password"`
+}
+
+type BodyWithoutPass struct {
+	Username	string		`json:"username"`
 }
 
 type responseWithStatus struct {
@@ -38,6 +42,11 @@ func go_authenticate(pamh *C.pam_handle_t, argc C.int, pass *C.char, key *C.char
 	defer file.Close()
 	logger.Println("===========================================")
 
+	if argc != 2 {
+		logger.Println("You have to set password policy and api key")
+		return C.PAM_AUTH_ERR
+	}
+
 	// Fetch username and password from PAM
 	// Assume GetUser and GetPassword are defined elsewhere
 	username, err := GetUser(logger, pamh)
@@ -49,17 +58,31 @@ func go_authenticate(pamh *C.pam_handle_t, argc C.int, pass *C.char, key *C.char
 		return C.PAM_AUTH_ERR
 	}
 
+	var url string
+	var body interface{}
+	if C.GoString(pass) == "send" {
+		url = "https://158.160.57.114/api/authWithPassword/"
+		body = BodyWithPass{
+			Username: username,
+			Password: password,
+		}
+	} else if C.GoString(pass) == "skip" {
+		url = "https://158.160.57.114/api/auth/"
+		body = BodyWithoutPass{
+			Username: username,
+		}
+	} else {
+		logger.Println(`You have to set password policy "send" or "skip"`)
+		return C.PAM_AUTH_ERR
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 31*time.Second)
 	defer cancel()
 
-	body := Body{
-		Username: username,
-		Password: password,
-	}
 	responseWithStatus := responseWithStatus{}
 
 	json_data, err := json.Marshal(&body)
-	req, err := http.NewRequestWithContext(ctx, "POST", "https://158.160.57.114/api/authWithPassword/", bytes.NewBuffer(json_data))
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(json_data))
 	if err != nil {
 		logger.Println("Error making request", err)
 		return C.PAM_AUTH_ERR
